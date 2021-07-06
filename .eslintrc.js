0406dbaf7e1d684a3da88d7977402dc1cf038481
TEEAARBEE <65780147+teeaarbee@users.noreply.github.com>
2021-07-06 17:01:08 +0530

    3b

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/babel', 'prettier/react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'react/prop-types': ['off'],
+    'prettier/prettier': 'error',
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+};
