eae9a71ad51728030dc7ae57b7c47c796595e209
TEEAARBEE <65780147+teeaarbee@users.noreply.github.com>
2021-07-28 20:03:06 +0530

    de2

--- .babelrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['babel-preset-gatsby'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-  ],
-};
